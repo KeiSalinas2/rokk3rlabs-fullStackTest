f108c5bb5c23c91bf835407ea01ecf8c38696d35
Keily Salinas <keisalinas2@gmail.com>
2016-09-01 15:09:15 -0400

    added serverjs

---
+++ server.js
@@ -0,0 +1,28 @@
+
+var express = require('express'),
+		app = express(),
+		port = 3001,
+    bodyParser = require('body-parser'),
+    mongoose = require('mongoose'),
+    config = require('./config/config');
+
+//view
+app.use(express.static(__dirname + '/build'));
+
+//connection url
+var uri = 'mongodb://' + config.db.host + '/' + config.db.database;
+
+// execute connection
+mongoose.connect(uri, { user: config.db.username, password: config.db.password }, function(err, res){
+  if(err) throw err;
+  console.log('Connected to db: ' + uri);
+});
+
+var Router = require('./app/router/router')(app);
+
+//start server
+var server = app.listen(port || 8080, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log('Running on http://%s:%s ', host, port);
+});
