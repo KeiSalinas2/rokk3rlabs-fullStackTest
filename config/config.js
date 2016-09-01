9e0cd50ca16e889076bfa1a28b0ab5dc760e6ed2
Keily Salinas <keisalinas2@gmail.com>
2016-09-01 15:11:16 -0400

    added config

---
+++ config/config.js
@@ -0,0 +1,13 @@
+var config = {
+  secretToken: 'KeyBoardCatStringSecret14_123_321',
+  port: 8080,
+  db: {
+    password: '',
+    username: '',
+    host: 'localhost',
+    database: 'appNode',
+    port: 27017
+  }
+};
+
+module.exports = config;
