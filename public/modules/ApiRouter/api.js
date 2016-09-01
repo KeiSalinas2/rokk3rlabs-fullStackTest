af08e47c9f8db03d91ae92de8592dec314ce1462
Keily Salinas <keisalinas2@gmail.com>
2016-09-01 15:28:28 -0400

    added api router

---
+++ public/modules/ApiRouter/api.js
@@ -0,0 +1,10 @@
+angular.module('app.services').service('ApiService', ['$http', function($http) {
+
+    'use strict';
+    var url = 'http://localhost:3001/';
+
+    this.apiTest = function(){
+      return $http.get(url + 'users/');
+    };
+
+  }]);
