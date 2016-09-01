d2570b10b6e19dd33fc4900e188f5f6812f702ac
Keily Salinas <keisalinas2@gmail.com>
2016-09-01 15:46:33 -0400

    added routers

---
+++ app/router/router.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var router = express.Router();
+
+var Controllers = require('../controllers');
+
+function Router(app, passport) {
+  // user routes
+  app.use('/users', Controllers.Users);
+
+  //The 404 Route (ALWAYS Keep this as the last route)
+  app.use(function(req, res) {
+    res.status(404).send({ code: 404, success: false, key: true, message: 'Not found' });
+  });
+}
+
+module.exports = Router;
